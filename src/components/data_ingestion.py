2b966d5e35a746fe51e5ce5ae36694e5f600d118
yitayew solomon <yitayewsolomon3@gmail.com>
2023-07-28 16:28:42 +0300

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,12 +3,15 @@ import sys
 from src.exception import customException
 from src.logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-#from src.components.model_trainer import ModelTrainerConfig
-#from src.components.model_trainer import ModelTrainer
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -44,14 +47,14 @@ class DataIngestion:
 
             )
         except Exception as e:
-            raise customException(e,sys)
+            raise CustomException(e,sys)
         
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    #train_arr,test_arr=data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    #modeltrainer=ModelTrainer()
-    #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
