db25bb0c8679758fa1ae0cce02b4d5d5026d503a
yitayew solomon <yitayewsolomon3@gmail.com>
2023-08-01 18:35:42 +0300

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,6 @@ import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
-
 from src.exception import customException
 
 def save_object(file_path, obj):
@@ -15,7 +14,7 @@ def save_object(file_path, obj):
 
         os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path, "wb") as file_obj:
+        with open(file_path, "rb") as file_obj:
             pickle.dump(obj, file_obj)
 
     except Exception as e:
@@ -35,7 +34,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            model.fit(X_train, y_train)  # Train model
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
